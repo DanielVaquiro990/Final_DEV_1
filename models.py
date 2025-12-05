75c03576df2fd3b203e572f1a34b19f0be481afb
labsis <labsis@debian.FI-LabSistemas.ucatolica.loc>
2025-12-05 04:49:22 -0500

    schemas ya completo y ubicacion archivos en carpetas

--- models.py
+++ models.py
@@ -6,39 +6,38 @@ from database import Base
 class Jugador(Base):
     __tablename__ = "Jugador"
     
-    numero = Column(Integer, primary_key=True, index=True)
+    
+    id = Column(Integer, primary_key=True, index=True)
+    numero = Column(Integer, index=True)
     nombre = Column(String(100), nullable=False)
     fechaNacimiento = Column(Integer, nullable=False)
     Nacionalidad = Column(String(100), nullable=False)
     altura =  Column(Integer, nullable=False)
     peso = Column(Integer, nullable=False)
     pieDominante = Column(String(10), nullable=False)
-    Posicion
+    Posicion = Column(String(100), nullable=False)
+
     pass
 
 
 class Estadistica(Base):
     __tablename__ ="Estadistica"
-
+    
+    id = Column(Integer, primary_key=True, index=True)
     minutos = Column(Integer, nullable=False)
     goles = Column(Integer, nullable=False)
     faltas = Column(Integer, nullable=False)
-    tarjetas 
+    CantidadTarjetas = Column(Integer, nullable=False)
     pass
 
 
 class Partido(Base):
     __tablename__ = "Partido"
-
-    Nombrerival = Column(String(100), nullable=False) 
+    id = Column(Integer, primary_key=True, index=True)
+    NombreRival = Column(String(100), nullable=False) 
     fecha = Column(DateTime, default=datetime.utcnow)
     golesLocal =  Column(Integer, nullable=False)
     golesRival = Column(Integer, nullable=False)
-
-    
-    
-    
-    
     pass
 
 
--- schemas.py
+++ schemas.py
@@ -15,11 +15,45 @@ class JugadorBase(BaseModel):
     posicion = str
 
 
+class JugadorCreate(JugadorBase):
+    pass
 
+
+class Jugador(JugadorBase):
+    id : int
+    class Config:
+        orm_mode = True
 
 #-------------Estadistica----------
 
+class EstadisticaBase(BaseModel):
+    minutos : int
+    goles : int
+    faltas : int
+    CantidadTarjetas : int
+
+class EstadisticaCreate(EstadisticaBase):
+    pass
+
+class Estadistica(EstadisticaBase):
+    id : int
+    class Config:
+        orm_mode = True
 
 
 
 #-----------------Partido---------------
+
+class PartidoBase (BaseModel):
+    NombreRival : str
+    fecha : date
+    golesLocal : int
+    golesRival : int
+
+class ParitdoCreate(PartidoBase):
+    pass
+
+class Partido(PartidoBase):
+    id : int
+    class Config:
+        orm_mode = True
